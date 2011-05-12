8e04bf64c7e91e2f07bc72079dc88f432dc5ce21
Nils Maier <maierman@web.de>
2011-05-12 03:31:38 +0200

    Switch thread implementation to PRThread

--- AsyncFileOutputStream/thread.c
+++ AsyncFileOutputStream/thread.c
@@ -3,44 +3,20 @@
  * and related or neighboring rights to this work.
  ************************************************************************/
 
-#include <windows.h>
+#include <prthread.h>
 #include "thread.h"
-
-typedef struct __thread_proc_t {
-  thread_proc_t start;
-  void *param;
-} _thread_proc_t;
-
-static DWORD WINAPI _thread_proc(LPVOID *param) {
-  _thread_proc_t *proc = (_thread_proc_t*)param;
-  proc->start(proc->param);
-  free(param);
-  return 0;
-}
 
 thread_t thread_create(thread_proc_t start, void *param) {
   thread_t *rv;
-  _thread_proc_t *proc = (_thread_proc_t*)malloc(sizeof(_thread_proc_t));
-
-  if (!proc) {
-    return NULL;
-  }
-
-  proc->start = start;
-  proc->param = param;
-
-  rv = (thread_t)CreateThread(
-    NULL,
-    0,
-    _thread_proc,
-    (LPVOID*)proc,
-    0,
-    NULL
+  return (thread_t)PR_CreateThread(
+    PR_USER_THREAD,
+    start,
+    param,
+    PR_PRIORITY_LOW,
+    PR_GLOBAL_THREAD,
+    PR_JOINABLE_THREAD,
+    0
     );
-  if (!rv) {
-    free(proc);
-  }
-
   return rv;
 }
 
@@ -48,12 +24,9 @@ int thread_join(thread_t thread) {
   if (!thread) {
     return 0;
   }
-  return WaitForSingleObject((HANDLE)thread, INFINITE) == WAIT_OBJECT_0;
+  return PR_JoinThread((PRThread*)thread) == PR_SUCCESS;
 }
 
 void thread_destroy(thread_t thread) {
-  if (!thread) {
-    return;
-  }
-  CloseHandle((HANDLE)thread);
+  /* no op */
 }
